2827a12a5da63fe27cdeda20bb9f5db6a2d2ed52
rungec <c.runge1@uq.edu.au>
2018-01-03 15:29:08 -0800

    prelim exploration of keywords

--- Grasslands_lit_review_analysis.r
+++ Grasslands_lit_review_analysis.r
@@ -6,6 +6,8 @@ setwd("D:/Box Sync/Projects/Other Projects/Ongoing/Grasslands_feedingtheworld/An
 
 library(bib2df)
 library(ggplot2)
+library(dplyr)
+library(stringr) #str_trim
 
 ###################
 #LOAD DATA
@@ -15,13 +17,31 @@ filelist <- list.files("180103/", pattern = 'WOS_2_core.*\\.bib', full.names=TRU
 
 bibDF <-do.call(rbind, lapply(filelist, function(x) bib2df(x)))
 
-test <- bib2df(filelist[1])
-test1 <- bib2df(filelist[1])[,1:47]
-head(test1$KEYWORDS)
-
 #need to wait for the issue to be fixed in github before I can do anything else
 https://cran.rstudio.com/web/packages/bib2df/vignettes/bib2df.html
 
+
+
+
+
 ###################
-#PLOTS
+#Data summary
 ###################
+
+test <- bib2df(filelist[1])
+test1 <- bib2df(filelist[1])[,1:47]
+test2 <- test1[!is.na(test1$CATEGORY),]
+head(test2$KEYWORDS)
+keywordlist <- lapply(test2$KEYWORDS, function(x){ 
+                                splitx <- strsplit(x, ";") #each keyword item in list
+                                splity <- lapply(splitx, function(y) tolower(str_trim(y))) #trim whitespace and convert to lowercase
+                                return(unlist(splity))
+                                })
+
+#count keywords
+
+keywordcount <- keywordlist %>% unlist() %>% as.factor() %>% table()
+
+head(keywordcount)
+write.csv(keywordcount, "keywordcount.csv")
+
